e2ef404caf5e46fa01acf7972fa5711cd9b8ad74
schirmann <cshirmann.dev@gmail.com>
2021-02-10 21:39:48 -0300

    Curso LaunchBase condicionais

---
+++ index.js
@@ -0,0 +1,28 @@
+// criar um programa que calcula media
+// das notas ente os alunos e envia
+// mensagemd e calculo de media
+
+const aluno01 = 'Mayk'
+const notaAluno01 = 9.0
+
+const aluno02 = 'Diego'
+const notaAluno02 = 10
+
+const aluno03 = 'Fulano'
+const notaAluno03 = 2
+
+
+const media = (notaAluno01 + notaAluno02 + notaAluno03) / 3
+
+// se a media for maior que 5 parabenizar a turma
+if (true) {
+ console.log(`a nota foi de ${media}. Parabens`)
+
+
+}  else {
+
+console.log('a media é menor que 5')
+
+}
+
+console.log(media < 5)
