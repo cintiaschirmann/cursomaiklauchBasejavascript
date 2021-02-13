2925300550c8bee4f81abd04c5ca648d32bdce62
schirmann <cshirmann.dev@gmail.com>
2021-02-13 17:47:47 -0300

    Curso LaunchBase Funçoes e metodos

--- index.js
+++ index.js
@@ -3,7 +3,7 @@
 // mensagem e calculo de media
 
 
-const alunos = [
+const alunosDaTurmaA = [
   {
     nome: "Mayk",
     nota: 9.8
@@ -18,34 +18,50 @@ const alunos = [
   }
 ]
 
-const nomesDeAlunos = ["Mayk", "Diego", "Fulano"]
+const alunosDaTurmaB = [
+  {
+    nome: "Cleiton",
+    nota: 10
+  },
+  {
+    nome: "Robson",
+    nota: 10
+  },
+  {
+    nome: "Siclano",
+    nota: 2
+  }
+]
 
+function calculaMedia(alunos) {
 
-const media = ( alunos[0].nota + alunos[1].nota + alunos[2].nota) / 3
-
-// se a media for maior que 5 parabenizar a turma
-if (media > 5) {
- console.log(`a nota foi de ${media}. Parabens`)
-
-
-}  else {
-
-console.log('a media é menor que 5')
+return ( alunos[0].nota + alunos[1].nota + alunos[2].nota) / 3
 
 }
 
-console.log(media < 5
-  )
+const media1 = calculaMedia (alunosDaTurmaA)
+const media2 = calculaMedia (alunosDaTurmaB)
 
 
-  const idade = 18
 
-  //verificar se a idade é igual ou maior de 18 anos
-  //se sim, deioxar entrar, se nao, bloquear entrada
-  // se a pessoa tiver 17 anos 
-  //avisar pra voltar quando fizer 18 anos
-  if (!(idade>=18) || idade === 17) {
-    console.log ('bloquear entrada')
-  } else {
-    console.log ('deixar entrar')
-  }
+
+function enviaMensagem(media, turma) {
+  // se a media for maior que 5 parabenizar a turma
+if (media > 5) {
+  console.log(`a media da turma ${turma} foi de ${media}. Parabens`)
+ }  else {
+ 
+ console.log(`a media da turma ${turma} é menor que 5`)
+ 
+ }
+ 
+ console.log(media < 5
+   )
+ 
+}
+
+//PARAMETROS DA FUNÇÃO
+enviaMensagem(media1, 'turmaA')
+enviaMensagem(media2, 'turmaB')
+
+
